044be4c91e81f73f263b3bc521a51b0572e4bfbb
crschung <crschung@gmail.com>
2023-01-17 17:37:11 -0800

    updated rust synthesizer and add python sinusoid

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ CSC 484D: Synthesizer Programming Weekly Log
 Assignment 1: Starting first assignment with intention in learning how to code in Rust and Python. As someone who has never coded in these two popular languages,
 this is a perfect opportunity to learn how the languages work.
 
+Jan 17, 2023: Successfully implemented a wavetable oscillator based off of https://thewolfsound.com/sound-synthesis/wavetable-synth-in-rust/. Can take inputs of MIDI notes into an array and plays them alongside an array of how long to play each note
+
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
-fn get_frequency(value:i32) -> f32{
-    // use std::io::{stdin,stdout,Write};
-
-    // let mut s=String::new();
-    // print!("Please enter some text: ");
-    // let _=stdout().flush();
-    // stdin().read_line(&mut s).expect("Did not enter a correct string");
-
-    // if let Some('\n')=s.chars().next_back() {
-    //     s.pop();
-    // }
-    // if let Some('\r')=s.chars().next_back() {
-    //     s.pop();
-    // }
-    // let x: f32= s.trim().parse().expect("Input not an integer");
-    // println!("You typed: {}",s);
-    let power = (value as f32-69.00)/12.00;
-    let frequency:f32 = (440.00*2.00_f32.powf(power)).into();
-    println!("Frequency of {} is: {:.2}", value,frequency);
-    return frequency;
-}
-
 use core::time::Duration;
-use rodio::{OutputStream, source::Source};
-
+use rodio::{OutputStream, source::Source, Sink};
+use rodio::source::{SineWave};
+use std::io::{stdin,stdout,Write, prelude::*,BufReader};
+use std::{fs::File, path::Path};
 
 struct WavetableOscillator {
     sample_rate: u32,
@@ -31,6 +11,56 @@ struct WavetableOscillator {
     index_increment: f32,
 }
 
+struct Sawtooth {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+struct Pulse {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+struct Triangle {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+struct Noise {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+struct Score{
+    message: String,
+    time: u64,
+    midi_note: i32,
+    unknown: i32
+}
+
+impl Score {
+    fn note_status(){
+
+    }
+    fn set_frequency(){
+
+    }
+    fn new(message: String, time: u64, midi_note: i32) -> Score{
+        return Score {
+            message: message,
+            time: time,
+            midi_note: midi_note,
+            unknown: 0
+        };
+    }
+    fn set_time(){
+
+    }
+}
 impl WavetableOscillator {
     fn new(sample_rate: u32, wave_table: Vec<f32>) -> WavetableOscillator {
         return WavetableOscillator {
@@ -88,6 +118,21 @@ impl Iterator for WavetableOscillator {
     }
 }
 
+fn get_frequency(value:i32) -> f32{
+    let power = (value as f32-69.00)/12.00;
+    let frequency:f32 = (440.00*2.00_f32.powf(power)).into();
+    println!("Frequency of {} is: {:.2}", value,frequency);
+    return frequency;
+}
+
+//reads a simple score language file 
+fn read_lines() -> Vec<String>{
+    let file = File::open("src/input.txt").expect("No file named input.txt was found");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l|l.expect("Couldn't parse line"))
+        .collect()
+}
 
 fn main() {
     println!("Never Gonna Give You Up!");
@@ -96,17 +141,21 @@ fn main() {
     // let b = [3000];
     //part 2 (Plays Never Gonna Give You Up by Rick Astley)
     let a = [67,69,72,69,76,76,74,67,69,72,69,74,74,72];
-    let b = [150,150,150,150,300,300,450,150,150,150,150,300,300,450];
+    let b = [200,200,200,200,400,400,500,200,200,200,200,400,400,500];
     //part 3
     //input txt file
-    //parse file
+    //parse file into array
     //assign frequency
     //noteon play freq for n ms
     //noteoff play -1 for n ms
+    
+    let lines = read_lines();
+    for line in lines {
+        println!("{}",line);
+    }
 
 
-
-    for it in a.iter().zip(b.iter()){
+    for it in a.iter().zip(b.iter()){ 
         let (ai,bi) = it;
         let wave_table_size = 64;
         let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
@@ -115,9 +164,20 @@ fn main() {
             wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
         }
         let mut oscillator = WavetableOscillator::new(44100, wave_table);
-        oscillator.set_frequency(get_frequency(*ai));
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let _result = stream_handle.play_raw(oscillator.convert_samples());
-        std::thread::sleep(std::time::Duration::from_millis(*bi));
+        if *ai >= 0 {
+            oscillator.set_frequency(get_frequency(*ai));
+        }
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            let sink = Sink::try_new(&stream_handle).unwrap();
+            //let _result = stream_handle.play_raw(oscillator.convert_samples());
+            // let val: u32 = (*ai).try_into().unwrap();
+            println!("{}",*ai);
+            let test = SineWave::new(get_frequency(*ai)as u32).take_duration(Duration::from_millis(*bi)).amplify(0.20);
+            sink.append(test);
+            println!("{}",sink.volume());
+            sink.set_volume(0.5);
+            println!("{}",sink.volume());
+            sink.sleep_until_end();
+            //std::thread::sleep(std::time::Duration::from_millis(*bi));
     }
 }
